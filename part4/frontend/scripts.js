6ca6a1c683f913701175190dfce24d1cd942f2b6
Adel <adelmejrissi@gmail.com>
2025-11-17 19:05:15 +0100

    feat: added code that makes the view details button functional

--- part4/frontend/scripts.js
+++ part4/frontend/scripts.js
@@ -11,6 +11,9 @@ function generateCard(place) {
 	const detailBtn = document.createElement("a");
 	detailBtn.className = "details-button";
 	detailBtn.innerText = "View Details";
+	detailBtn.id = place.id
+
+	detailBtn.addEventListener("click", viewDetails);
 
 	card.appendChild(cardName);
 	card.appendChild(cardPrice);
@@ -19,6 +22,33 @@ function generateCard(place) {
 	return card;
 }
 
+function viewDetails() {
+	fetch("http://127.0.0.1:5000/api/v1/places/" + this.id)
+		.then(response => response.json())
+		.then(data => {
+			// fetching button parent
+			let card = document.getElementById(this.id).parentElement
+
+			// creating a div for card details
+			let cardDetails = document.createElement("div");
+			cardDetails.className = "place-details"
+
+			// creating paragraph representing the host
+			let cardHost = document.createElement("p");
+
+			// creating paragraph representing the description
+			let cardInfo = document.createElement("p");
+			cardInfo.className = "place-info";
+			cardInfo.innerText = data.description
+
+			cardDetails.appendChild(cardHost);
+
+			card.appendChild(cardDetails);
+			card.appendChild(cardInfo);
+			this.remove()
+		})
+		.catch(err => console.error("Error:", err));
+}
 
 document.addEventListener('DOMContentLoaded', () => {
 	fetch("http://127.0.0.1:5000/api/v1/places/")
@@ -27,8 +57,7 @@ document.addEventListener('DOMContentLoaded', () => {
 			const placeList = document.getElementById("places-list");
 			for (const value of data) {
 				let card = generateCard(value);
-				placeList.append(card);
+				placeList.appendChild(card);
 			}
 		})
-
 });
