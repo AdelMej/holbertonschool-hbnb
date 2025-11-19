2f39e0721ed48982ee3fb20c282ed11316911ac1
Adel <adelmejrissi@gmail.com>
2025-11-19 17:12:38 +0100

    feat: cleaned up my files now uses classes for convinience also everything works you can't review without being logged in you get redirected if you try to fetch a place that doesn't exist

--- part4/frontend/js/index.js
+++ part4/frontend/js/index.js
@@ -1,14 +1,17 @@
 import { Cookie } from "./utils.js";
+import { FetchPlaceReviews } from "./models/review.js";
+import { FetchPlace } from "./models/place.js";
 
 function generateCard(place) {
 	let card = document.createElement("div");
 	card.className = "place-card";
 
-	let cardName = document.createElement("p");
-	cardName.innerText = "name: " + place.title;
+
+	let cardName = document.createElement("h1");
+	cardName.innerText = place.title;
 
 	let cardPrice = document.createElement("p");
-	cardPrice.innerText = "Price per night: " + place.price;
+	cardPrice.innerText = "Price per night: " + place.price + "€";
 
 	const detailBtn = document.createElement("a");
 	detailBtn.className = "details-button";
@@ -22,7 +25,12 @@ function generateCard(place) {
 	card.dataset.price = place.price;
 	card.dataset.name = place.title;
 
-	card.appendChild(cardName);
+	// creating a link that redirect to place page
+	let placeLink = document.createElement("a");
+	placeLink.href = "http://127.0.0.1:8000/place.html?id=" + place.id;
+	placeLink.appendChild(cardName);
+
+	card.appendChild(placeLink);
 	card.appendChild(cardPrice);
 	card.appendChild(detailBtn);
 
@@ -30,120 +38,82 @@ function generateCard(place) {
 }
 
 async function viewDetails() {
-	try {
-		const response = await fetch("http://127.0.0.1:5000/api/v1/places/" + this.id)
+	let card = this.parentElement;
 
-		if (!response.ok) {
-			console.error("HTTP error: ", response.status);
-			return;
-		}
+	let place = new FetchPlace(this.id);
+	await place.load();
 
-		const data = await response.json();
+	// Create details container
+	const details = document.createElement("div");
+	details.className = "place-details";
 
-		// fetching button parent
-		let card = document.getElementById(this.id).parentElement
+	// Host
+	const host = document.createElement("p");
+	host.innerText = "Host: " + place.getOwner(place.getOwner().first_name + " " + place.getOwner().last_name);
 
-		// creating a div for card details
-		let cardDetails = document.createElement("div");
-		cardDetails.className = "place-details";
-
-		// creating paragraph representing the host
-		let cardHost = document.createElement("p");
-		cardHost.innerText = "Host: " + data.owner.first_name + " " + data.owner.last_name;
-
-		// creating paragraph representing the price
-		let cardPrice = document.createElement("p");
-		cardPrice.className = "price";
-		cardPrice.innerText = "Price: " + data.price + " €";
-
-		// creating amenities div and paragraph
-		let cardAmenities = document.createElement("div");
-		for (const amenity of data.amenities) {
-			let cardAmenty = document.createElement("p")
-			cardAmenty.innerText = amenity.name;
-			cardAmenities.appendChild(cardAmenty);
-		}
-
-		// add information to the details
-		cardDetails.appendChild(cardHost);
-		cardDetails.appendChild(cardAmenities);
-
-		let cardReview = document.createElement("div");
-		generateReview(this.id, cardReview)
-
-		// creating paragraph representing the description
-		let cardInfo = document.createElement("p");
-		cardInfo.className = "place-info";
-		cardInfo.innerText = data.description
-
-		card.appendChild(cardDetails);
-		card.appendChild(cardInfo);
-		if (cardReview) {
-			card.appendChild(cardReview);
-		}
-
-		this.remove()
-
+	// Amenities
+	const amenities = document.createElement("div");
+	for (const a of place.getAmenities()) {
+		const p = document.createElement("p");
+		p.innerText = a.name;
+		amenities.appendChild(p);
 	}
-	catch (err) {
-		console.error("Error: ", err);
-	}
-};
+
+	// Description
+	const desc = document.createElement("p");
+	desc.className = "place-info";
+	desc.innerText = place.getDescription();
+
+	// Reviews
+	const reviewSection = document.createElement("div");
+	generateReview(this.id, reviewSection);
+
+	// Append all
+	details.appendChild(host);
+	details.appendChild(amenities);
+	details.appendChild(desc);
+	details.appendChild(reviewSection);
+
+	// Reduce button
+	const reduceBtn = document.createElement("button");
+	reduceBtn.innerText = "Hide details";
+
+	reduceBtn.onclick = () => {
+		details.classList.add("hidden");
+		this.classList.remove("hidden");
+	};
+
+	details.appendChild(reduceBtn);
+
+	// Insert into card
+	card.appendChild(details);
+
+	// Hide "view details" button
+	this.classList.add("hidden");
+}
 
 async function generateReview(place_id, card) {
-	try {
-		const response = await fetch("http://127.0.0.1:5000/api/v1/places/" + place_id + "/reviews");
-		if (response.status === 404) {
-			if (Cookie.get("token")) {
-				let reviewBtn = document.createElement("a");
-				reviewBtn.href = "/add_review.html";
-				reviewBtn.innerText = "add a review";
 
-				card.appendChild(reviewBtn);
-			}
+	let placeReviews = new FetchPlaceReviews(place_id);
+	await placeReviews.load();
 
-			let reviews = document.createElement("p");
-			reviews.innerText = "No review found";
+	let divReview = document.createElement("div");
 
-			card.appendChild(reviews);
-			return;
-		}
+	for (const review of placeReviews.getAll()) {
+		let reviewTitle = document.createElement("h1");
+		reviewTitle.innerText = review.title;
 
-		if (!response.ok) {
-			console.error("HTTP error:", response.status);
-			return;
-		}
+		let reviewText = document.createElement("p");
+		reviewText.innerText = review.text;
 
-		const data = await response.json();
+		let reviewRating = document.createElement("p");
+		reviewRating.innerText = review.rating;
 
-		if (!data) return;
-		let divReview = document.createElement("div");
-
-		if (Cookie.get("token")) {
-			let reviewBtn = document.createElement("a");
-			reviewBtn.href = "/add_review.html";
-			reviewBtn.innerText = "add a review";
-			divReview.appendChild(reviewBtn);
-		}
-
-		for (const review of data) {
-			let reviewTitle = document.createElement("p");
-			reviewTitle.innerText = review.title;
-
-			let reviewText = document.createElement("p");
-			reviewText.innerText = review.text;
-
-			let reviewRating = document.createElement("p");
-			reviewRating.innerText = review.rating;
-
-			divReview.appendChild(reviewTitle);
-			divReview.appendChild(reviewText);
-			divReview.appendChild(reviewRating);
-		}
-		card.appendChild(divReview);
-	} catch (err) {
-		console.error("Error: ", err);
+		divReview.appendChild(reviewTitle);
+		divReview.appendChild(reviewText);
+		divReview.appendChild(reviewRating);
 	}
+	card.appendChild(divReview);
 };
 
 
@@ -180,10 +150,10 @@ function filterPlace() {
 	for (const place of places) {
 		const price = Number(place.dataset.price);
 		if (price < value) {
-			place.style.display = "none";
+			place.classList.add("hidden");
 		}
 		else {
-			place.style.display = "";
+			place.classList.remove("hidden");
 		}
 	}
 }
